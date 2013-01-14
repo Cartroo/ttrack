6be412b701b9c9dd0aabe008938854e4fd6e838f
Andy Pearce <andy@andy-pearce.com>
2013-01-14 14:37:31 +0000

    Updates to fix PyPI issues
    This commit fixes a broken classifier line in `setup.py` and also adds
    code to filter `README.rst` to remove any Sphinx-specific directives.
    Currently this only includes `highlight`.

--- cmdparser/setup.py
+++ cmdparser/setup.py
@@ -4,6 +4,16 @@ from setuptools import setup
 from cmdparser import __version__
 
 
+remove_directives = ("highlight",)
+
+def filter_directives(in_iter):
+    for line in in_iter:
+        if line.startswith("..") and "::" in line:
+            if line.split("::", 1)[0][2:].strip() in remove_directives:
+                continue
+        yield line
+
+
 setup(name="cmdparser",
       version=__version__,
       author="Andy Pearce",
@@ -12,13 +22,13 @@ setup(name="cmdparser",
       url="http://cartroo.github.com/ttrack/cmdparser.html",
       license="LICENSE.txt",
       description="Command parsing extensions to the cmd module.",
-      long_description=open("README.rst").read(),
+      long_description="".join(filter_directives(open("README.rst"))),
       classifiers=[
           "Development Status :: 5 - Production/Stable",
           "Intended Audience :: Developers",
           "License :: OSI Approved :: MIT License",
           "Natural Language :: English",
-          "Operating System :: OS Independent"
+          "Operating System :: OS Independent",
           "Programming Language :: Python",
           "Programming Language :: Python :: 2",
           "Topic :: Software Development :: Libraries",
